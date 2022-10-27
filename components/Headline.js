6bdae4e99c79879ecbb7ddbc8f7450752acdb178
shinka401 <shinka401unauthorized@gmail.com>
2022-10-27 21:37:43 +0900

    ♻️ メイン部分をコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -1,17 +1,13 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export function Headline(props) {
-  console.log(props.title)
   return (
     <div>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">{props.page} page</a>
-        </h1>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">{props.page} page</a>
+      </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/{props.page}.js</code>
-        </p>
+      <p className={styles.description}>Get started by editing {props.children}</p>
     </div>
-  )
+  );
 }
---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import { Headline } from "../components/Headline";
+import { Links } from "../components/Links";
+import styles from "../styles/Home.module.css";
+
+export function Main(props) {
+  return (
+      <main className={styles.main}>
+        <Headline
+        page="about"
+        >
+        <code className={styles.code}>pages/{props.page}.js</code>
+        </Headline>
+
+        <Links />
+      </main>
+  );
+}
