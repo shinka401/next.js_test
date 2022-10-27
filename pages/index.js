780df83233b089f5d69dd461ed66f09b28ea3988
shinka401 <shinka401unauthorized@gmail.com>
2022-10-27 21:15:19 +0900

    ♻️ export deforutoの削除

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import { Links } from '../components/Links'
 import { Headline } from '../components/Headline'
 
-export function Home() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
