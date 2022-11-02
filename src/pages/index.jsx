6937eafa1670d3180de0f4ff1f506b929b182c0b
shinka401 <shinka401unauthorized@gmail.com>
2022-11-02 15:35:53 +0900

    ✨ 講座用のuseefectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
@@ -12,6 +12,13 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   }, []);
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
