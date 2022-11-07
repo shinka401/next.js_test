2b3d6c9ad8416c9fef918ec9d2aca897d9f0fb7d
shinka401 <shinka401unauthorized@gmail.com>
2022-11-07 19:04:03 +0900

    ⚡️ useCallebackとuseEdectの第二引数に関して説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import {  useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
@@ -7,14 +7,20 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    () => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
+    console.log(`マウント時: ${count}`);
     document.body.style.backgroundColor = "lightblue";
     return () => {
+      console.log(`アンマウント時: ${count}`);
       document.body.style.backgroundColor = "";
     };
   }, []);
