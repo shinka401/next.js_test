450ec11390edd5be2bf4152550e67319cd021668
shinka401 <shinka401unauthorized@gmail.com>
2022-11-12 22:22:20 +0900

    ♻️ useStateをまとめた

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,9 +1,11 @@
 import { useCallback, useEffect, useState } from "react";
 
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+    error: null,
+  });
 
   const getPosts = useCallback(async () => {
     try {
@@ -12,29 +14,42 @@ export const Posts = () => {
         throw new Error("エラーが発生");
       }
       const json = await res.json();
-      setPosts(json);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          data: json,
+          loading: false,
+        };
+      });
     } catch (error) {
-      setError(error);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          loading: false,
+          error,
+        };
+      });
     }
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  if (loading) {
+
+  console.log("foo");
+  if (state.loading) {
     return <div>{"ローディング中"}</div>;
   }
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div>データがありません</div>;
   }
 
   return (
     <ol>
-      {posts.map((item) => {
+      {state.data.map((item) => {
         return <li key={item.id}>{item.title}</li>;
       })}
     </ol>
