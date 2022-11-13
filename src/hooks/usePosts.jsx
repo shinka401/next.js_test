c6671bf0c566e12988378759a61d5059a3ea14e0
shinka401 <shinka401unauthorized@gmail.com>
2022-11-13 10:32:52 +0900

    🎨 replace to swr.

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
+
+export const usePosts = () => {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  return {
+    data,
+    error,
+    isLoading: !error && !data,
+    isEmpty: data && data.length === 0,
+  };
+};
